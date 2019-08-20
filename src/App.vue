383779298af3470b6f70ee027c32edffbb15d0ec
Jeffrey Huang <jeffreyhuang0331@gmail.com>
2019-08-20 11:55:28 +0800

    start

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
   components: {
-    HelloWorld
+
+  },
+  data () {
+    return {
+      notifications: []
+    }
+  },
+  mounted () {
+    this.notifications = [
+      {
+        id: 1,
+        type: 'post-replied',
+        user: {
+          name: 'Alex'
+        }
+      },
+      {
+        id: 2,
+        type: 'post-upvoted',
+        user: {
+          name: 'Billy'
+        }
+      }
+    ]
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
